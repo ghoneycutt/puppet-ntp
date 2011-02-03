1a1392f82c5b95efbca16e1cdca8e9e90ff8610c
hakamadare <shuff@vecna.org>
2011-02-03 12:32:54 -0500

    case statement to support operation on Debian-based and RHEL-based systems
    	modified:   manifests/init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,18 +10,34 @@
 #
 class ntp {
 
-    package { "ntp": }
+    # the ntp package and service has a different name under RHEL vs. Debian
+    case $operatingsystem {
+        /(Debian|Ubuntu)/: { 
+            $ntp_package = "ntp" 
+            $ntp_service = "ntp" 
+        }
+        /(RedHat|CentOS|Fedora)/: { 
+            $ntp_package = "ntp" 
+            $ntp_service = "ntpd" 
+        }
+        default: { 
+            $ntp_package = "ntp" 
+            $ntp_service = "ntpd" 
+        }
+    } # case
+
+    package { $ntp_package: }
 
     file { "/etc/ntp.conf":
         mode    => "644",
         content => template("ntp/client-ntp.conf.erb"),
-        notify  => Service["ntpd"],
-        require => Package["ntp"],
+        notify  => Service[$ntp_service],
+        require => Package[$ntp_package],
     } # file
 
-    service { "ntpd":
+    service { $ntp_service:
         ensure  => running,
         enable  => true,
-        require => Package["ntp"],
+        require => Package[$ntp_package],
     } # service
 } # class ntp
